4b0c1a4ce4212ba3693c41a4ae6320f47e8b2d4b
Fauziahalsey <fauziaomala19@gmail.com>
2023-07-06 21:01:36 +0300

    initiaizing

--- Js/index.js
+++ Js/index.js
@@ -1,86 +1,107 @@
-// JavaScript (script.js)
+// Fetch more job listings when "Browse Jobs" button is clicked
+document.querySelector('.btn-primary').addEventListener('click', fetchMoreJobs);
 
-// Search functionality
-const searchForm = document.querySelector('.search-form');
-const searchInput = document.querySelector('.search-input');
-const jobListings = document.querySelector('.job-listings');
-const browseJobsButton = document.querySelector('.btn-browse-jobs');
-
-let page = 1;
-
-searchForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const searchTerm = searchInput.value.trim().toLowerCase();
-  if (searchTerm === '') return;
-
-  page = 1;
-  searchJobs(searchTerm);
-});
-
-browseJobsButton.addEventListener('click', function (e) {
-  const searchTerm = searchInput.value.trim().toLowerCase();
-  if (searchTerm === '') return;
-
-  page++;
-  searchJobs(searchTerm, page);
-});
-
-function searchJobs(searchTerm, page = 1) {
-  // Clear previous search results
-  if (page === 1) {
-    jobListings.innerHTML = '';
-  }
-
-  // Fetch jobs from API
-  fetch(`https://api.example.com/jobs?search=${searchTerm}&page=${page}`)
+function fetchMoreJobs() {
+  // Perform an AJAX request to the API endpoint
+  // Replace 'YOUR_API_KEY' with your actual API key
+  fetch('https://arbeitnow.com/api/job-board-api?api_key=YOUR_API_KEY')
     .then(response => response.json())
     .then(data => {
-      // Display search results
-      if (data.length === 0) {
-        if (page === 1) {
-          jobListings.innerHTML = '<p>No jobs found.</p>';
-        } else {
-          browseJobsButton.style.display = 'none';
-        }
-      } else {
-        data.forEach(job => {
-          const jobListing = createJobListing(job);
-          jobListings.appendChild(jobListing);
-        });
-
-        if (data.length < 10) {
-          browseJobsButton.style.display = 'none';
-        } else {
-          browseJobsButton.style.display = 'block';
-        }
-      }
+      // Process the retrieved job listings data
+      displayJobs(data);
     })
     .catch(error => {
-      console.log('An error occurred:', error);
-      jobListings.innerHTML = '<p>Failed to fetch jobs. Please try again later.</p>';
+      console.error('Error fetching job listings:', error);
     });
 }
 
-function createJobListing(job) {
-  const jobListing = document.createElement('div');
-  jobListing.classList.add('job-listing');
+// Display the fetched job listings
+function displayJobs(jobListings) {
+  const jobListingsContainer = document.querySelector('.job-listings');
 
-  const jobTitle = document.createElement('h3');
-  jobTitle.classList.add('job-title');
-  jobTitle.textContent = job.title;
+  // Clear the existing job listings
+  jobListingsContainer.innerHTML = '';
 
-  const jobCompany = document.createElement('p');
-  jobCompany.classList.add('job-company');
-  jobCompany.textContent = job.company;
+  // Create HTML elements for each job listing
+  jobListings.forEach(job => {
+    const jobListing = document.createElement('div');
+    jobListing.classList.add('job-listing');
 
-  const jobLocation = document.createElement('p');
-  jobLocation.classList.add('job-location');
-  jobLocation.textContent = job.location;
+    const jobTitle = document.createElement('h3');
+    jobTitle.classList.add('job-title');
+    jobTitle.textContent = job.title;
 
-  jobListing.appendChild(jobTitle);
-  jobListing.appendChild(jobCompany);
-  jobListing.appendChild(jobLocation);
+    const jobCompany = document.createElement('p');
+    jobCompany.classList.add('job-company');
+    jobCompany.textContent = job.company;
 
-  return jobListing;
+    const jobLocation = document.createElement('p');
+    jobLocation.classList.add('job-location');
+    jobLocation.textContent = job.location;
+
+    const applyButton = document.createElement('a');
+    applyButton.classList.add('btn-apply');
+    applyButton.textContent = 'Apply';
+    applyButton.href = job.applyUrl;
+
+    jobListing.appendChild(jobTitle);
+    jobListing.appendChild(jobCompany);
+    jobListing.appendChild(jobLocation);
+    jobListing.appendChild(applyButton);
+
+    jobListingsContainer.appendChild(jobListing);
+  });
 }
+
+// Submit email form
+document.querySelector('.contact-form').addEventListener('submit', submitEmail);
+
+function submitEmail(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const name = form.querySelector('#name').value;
+  const email = form.querySelector('#email').value;
+  const message = form.querySelector('#message').value;
+
+  // Perform an AJAX request to the API endpoint for submitting emails
+  // Replace 'YOUR_API_KEY' with your actual API key
+  fetch('https://arbeitnow.com/api/job-board-api/submit-email?api_key=YOUR_API_KEY', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, email, message })
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response data
+      if (data.success) {
+        alert('Email sent successfully!');
+        form.reset();
+      } else {
+        alert('Failed to send email. Please try again.');
+      }
+    })
+    .catch(error => {
+      console.error('Error submitting email:', error);
+    });
+}
+
+// Apply for job
+document.querySelectorAll('.btn-apply').forEach(button => {
+  button.addEventListener('click', applyForJob);
+});
+
+// function applyForJob(event) {
+//   event.preventDefault();
+
+//   const jobTitle = event.target.parentElement.querySelector('.job-title').textContent;
+
+//   // Perform an AJAX request to the API endpoint for applying to jobs
+//   // Replace 'YOUR_API_KEY' with your actual API key
+//   fetch('https://arbeitnow.com/api/job-board-api/apply-job?api_key=YOUR_API_KEY', {
+//     method: 'POST',
+//     headers: {
+//     //   'Content-Type': 'application/json'
+//     },
