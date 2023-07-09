fe16e94bb409b63ae193f72ef13eda021b4104a4
Fauziahalsey <fauziaomala19@gmail.com>
2023-07-09 13:22:26 +0300

    deletion and insertion of some codes

--- Js/index.js
+++ Js/index.js
@@ -1,38 +1,4 @@
-// document.onload = loadInitial_data()
 
-// async function fetchData(){
-//     let response= await fetch('./assets/db.json')
-//     let data= await response.json()
-//     return data;
-// }
-
-// function showJobs(data){
-//     let jobContainer= document.querySelector('.job-tile-container')
-//     let jobHTML= ""
-//     data.forEach(function (job){
-//         jobHTML += `
-//         <div class="job-tile">
-//                 <div class="job-tile-head">
-//                     <div class="logo"><img src="${job.logo}"/></div>
-//                     <div class="dots"><i class="fas fa-ellipsis-h"></i></div>
-//                 </div>
-//                 <div class="job-tile-title">${job.roleName}</div>
-//                 <div class="job-tile-desc">${job.requirements.content}</div>
-//                 <div class="btns">
-//                     <div class="apply">Apply</div>
-//                     <div class="message">Details</div>
-//                 </div>
-//             </div>
-//         `
-//     });
-//     jobContainer.innerHTML = jobHTML
-// }
-
-// async function loadInitial_data(){
-//     const data= await fetchData();
-//     showJobs(data);
-// }
-// Sample job data
 const jobs = [
     {
       "id": 1,
